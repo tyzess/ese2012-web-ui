052a8c147c3e143ac41d020bd42d3034123d8e97
Urs Zysset <UrsZysset@gmail.com>
2012-10-02 21:14:44 +0200

    finished "web-ui"

--- app/app.rb
+++ app/app.rb
@@ -18,17 +18,17 @@ class App < Sinatra::Base
 
   configure :development do
     urs = Traders::User.create("Urs")
-    oli = Traders::User.create("Oliver")
+    ese = Traders::User.create("ese")
     kenneth = Traders::User.create("Kenneth")
     adrian = Traders::User.create("Adrian")
 
     urs.save
-    oli.save
+    ese.save
     kenneth.save
     adrian.save
 
     urs.give_credits(1000000)
-    oli.give_credits(999)
+    ese.give_credits(999)
     kenneth.give_credits(1234)
     adrian.give_credits(45)
 
@@ -36,8 +36,8 @@ class App < Sinatra::Base
     urs.add_item(Traders::Item.create_active("Hut",13.55))
     urs.add_item(Traders::Item.create_active("Handy",301))
 
-    oli.add_item(Traders::Item.create_active("Velo",221))
-    oli.add_item(Traders::Item.create_active("GA",2500))
+    ese.add_item(Traders::Item.create_active("Velo",221))
+    ese.add_item(Traders::Item.create_active("GA",2500))
 
     adrian.add_item(Traders::Item.create("Schwedische Penispumpe",123) )
     adrian.add_item(Traders::Item.create_active("Huhn",22))
